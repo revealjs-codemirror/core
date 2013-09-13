c24e670bbb7e8dda3ddf30d558a56558c36824a1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-09-13 15:18:30 +0200

    initialize jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,6 +64,12 @@ module.exports = function(grunt) {
         files: '<%= jshint.lib_test.src %>',
         tasks: ['jshint:lib_test']
       }
+    },
+    jasmine: {
+      src: [ 'lib/<%= pkg.name %>.js' ],
+        options: {
+            specs: [ 'spec/setup_spec.js' ]
+        }
     }
   });
 
@@ -72,8 +78,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'jasmine', 'concat', 'uglify']);
 
 };
---
+++ spec/setup_spec.js
@@ -0,0 +1,6 @@
+/*global describe:false, it:false, expect:false*/
+describe('a Jasmine project', function(){
+    it('should run specifications', function(){
+	expect(true).toBe(true);
+    });
+});
