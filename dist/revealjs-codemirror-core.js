167d90f9b8b561b1b5e76627ddba6eec075d5b10
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-09-14 12:22:48 +0200

    add dist to repository

--- dist/revealjs-codemirror-core.js
+++ dist/revealjs-codemirror-core.js
@@ -1,4 +1,4 @@
-/*! revealjs-codemirror-core - v0.0.0 - 2013-09-13
+/*! revealjs-codemirror-core - v0.0.0 - 2013-09-14
 * Copyright (c) 2013 ; Licensed MIT */
 window.revealjscodemirror = (function(){
   return {
