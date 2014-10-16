abd25f323570bc0a8ac6dba9900b31c1126c9afd
dvberkel <daan.v.berkel.1980@gmail.com>
2014-10-16 21:55:58 +0200

    should create promises

--- spec/q_spec.js
+++ spec/q_spec.js
@@ -2,4 +2,16 @@ describe('q', function(){
 	it('should be defined', function(){
 		expect(Q).toBeDefined();
 	});
+
+	describe('promise', function(){
+		var promise;
+
+		beforeEach(function(){
+			promise = Q.defer().promise;
+		});
+
+		it('should be created', function(){
+			expect(promise).toBeDefined();
+		});
+	});
 });
