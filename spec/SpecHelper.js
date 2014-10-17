8a6e5def002035ca7cb13acebc777da059a0767f
dvberkel <daan.v.berkel.1980@gmail.com>
2014-10-17 03:00:23 +0200

    use latest grunt-contrib-jasmine

--- spec/SpecHelper.js
+++ spec/SpecHelper.js
@@ -1,10 +1,15 @@
-/*global beforeEach:false */
+/*global jasmine:false, beforeEach:false */
 beforeEach(function(){
-    this.addMatchers({
-        toIncludeStyle: function(expectedStyle){
-            var container = this.actual;
-            var style = container.getAttribute('style');
-            return style && style.indexOf(expectedStyle) !== -1;
+    jasmine.addMatchers({
+        toIncludeStyle: function(){
+			return {
+				compare: function(actual, expectedStyle){
+					var style = actual.getAttribute('style');
+					var result = {};
+					result.pass = style && style.indexOf(expectedStyle) !== -1;
+					return result;
+				}
+			};
         }
     });
 });
